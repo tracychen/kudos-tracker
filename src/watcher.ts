46a87369228e1910ef545ac40ae8665c405d95a3
Tracy Chen <tracychen0711@gmail.com>
2022-04-12 21:11:36 -0700

    Use Alchemy websocket with refresh to prevent connection drops

--- src/watcher.ts
+++ src/watcher.ts
@@ -7,8 +7,33 @@ export function watchKudosEvents() {
     dotenv.config();
 
     // Instantiate web3 with WebSocketProvider
-    console.log(`Using Polygon Websocket: ${process.env.POLYGON_WS_URL}`)
-    const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.POLYGON_WS_URL!));
+    console.log(`Using Polygon websocket url: ${process.env.POLYGON_WS_URL}`)
+
+    const web3 = new Web3()
+    // const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.POLYGON_WS_URL!));
+
+    // Refreshes provider instance and attaches event handlers
+    function refreshProvider(web3Obj: Web3, providerUrl: string) {
+        function retry() {
+            console.log(`Reconnecting web3 provider for url ${providerUrl}`)
+            refreshProvider(web3Obj, providerUrl)
+
+            return null
+        }
+
+        const provider = new Web3.providers.WebsocketProvider(providerUrl)
+
+        provider.on('end', () => retry())
+        provider.on('error', () => retry())
+
+        web3Obj.setProvider(provider)
+
+        console.log(`New Web3 provider initiated for url ${provider.connection.url}`)
+
+        return provider
+    }
+
+    refreshProvider(web3, process.env.POLYGON_WS_URL!)
 
     // Instantiate token contract object with JSON ABI and address
     const contract = new web3.eth.Contract(
@@ -22,5 +47,7 @@ export function watchKudosEvents() {
     .on('data', (event: any) => {
         console.log(event);
     })
-    .on('error', console.error);
+    .on('error', (error: any) => {
+        console.log(error);
+    })
 }
